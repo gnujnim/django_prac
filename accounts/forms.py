72e54312ff848072b58aa6ffe495dcffb3d2d109
gnujnim <minjeong0887921@gmail.com>
2023-03-13 19:23:24 +0900

    개인정보수정 및 패스워드 변경

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm, PasswordChangeForm as AuthPasswordChangeForm
+)
 from .models import User
 
 
@@ -21,3 +23,18 @@ class SignupForm(UserCreationForm):
             if qs.exists():
                 raise forms.ValidationError('이미 등록된 이메일 주소입니다')
         return email
+    
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['profile_img', 'first_name', 'last_name', 'website_url', 'bio', 'phone_number', 'gender']
+
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password2(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_password2 = super().clean_new_password2()
+        if old_password == new_password2:
+            raise forms.ValidationError('기존 패스워드와 다르게 입력해 주세요')
+        return new_password2
--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
+from django.core.validators import RegexValidator
 from django.db import models
 from django.template.loader import render_to_string
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', "Female"
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, blank=True, validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
+    profile_img = models.ImageField(blank=True, upload_to='accounts/profile_img/%Y/%m/%d',
+                                    help_text='48px * 48px 크기의 png/jpg 파일을 업로드 해주세요')
 
     def send_welcome_email(self):
         pass
